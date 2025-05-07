751533b2e971e696d7f7f52a50b1ccab2008549f
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2025-05-07 08:22:06 -0700

    Add getter and setter for cells and universe dimensions

--- src/lib.rs
+++ src/lib.rs
@@ -50,6 +50,18 @@ impl Universe {
         self.width
     }
 
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        let size = (width * self.height) as usize;
+        self.cells = FixedBitSet::with_capacity(size);
+    }
+
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        let size = (height * self.width) as usize;
+        self.cells = FixedBitSet::with_capacity(size);
+    }
+
     pub fn height(&self) -> u32 {
         self.height
     }
@@ -102,6 +114,19 @@ impl Universe {
             }
         }
         count
+    }
+}
+
+impl Universe {
+    pub fn get_cells(&self) -> &FixedBitSet {
+        &self.cells
+    }
+
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter().cloned() {
+            let idx = self.get_index(row, col);
+            self.cells.set(idx, true);
+        }
     }
 }
 
